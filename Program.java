05cbf8709324149f65ba423f5dbbb3096a4ffda0
Archimedes314 <63058147+Archimedes314@users.noreply.github.com>
2020-05-11 17:40:12 +0200

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,99 @@
+
+
+import java.util.*;
+public class Program {
+
+
+    public static List<Integer> riverSizes(int[][] matrix) {
+        List<Integer> riverSizes = new ArrayList<>();
+        boolean[][] visitedPoint = new boolean[matrix.length][matrix[0].length];
+        for(int x =0;x<matrix.length;x++){
+            for(int y=0;y<matrix[0].length;y++){
+                if(visitedPoint[x][y]){
+                    continue;
+                }
+                findRiverSize(x,y,matrix,visitedPoint,riverSizes);
+
+            }
+
+        }
+
+
+
+        // Write your code here.
+        return riverSizes;
+    }
+
+    private static void findRiverSize(int x, int y, int[][] matrix, boolean[][] visitedPoint,List<Integer> riverSizes) {
+
+        List<Integer[]> searchQueue = new ArrayList<>();
+        searchQueue.add(new Integer[] {x,y});
+        int OneCount=0;
+        while(!searchQueue.isEmpty()){
+
+            Integer[] currentPoint = searchQueue.get(0);
+            searchQueue.remove(0);
+            int i=currentPoint[0];
+            int j= currentPoint[1];
+            if(visitedPoint[i][j]){
+                continue;
+            }
+            if(matrix[i][j]==0){
+                continue;
+            }
+            visitedPoint[i][j]=true;
+            OneCount++;
+            List<Integer[]> neighbours = getNeighbouringPoints(i,j,matrix);
+            searchQueue.addAll(neighbours);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        if(OneCount>0){
+            riverSizes.add(OneCount);
+
+        }
+
+
+
+
+
+
+    }
+
+    private static List<Integer[]> getNeighbouringPoints(int i, int j, int[][] matrix) {
+        List<Integer[]> neighbourList = new ArrayList<>();
+        if(i>0){
+            neighbourList.add(new Integer[] {i-1,j});
+
+        }
+        if(i<matrix.length-1){
+            neighbourList.add(new Integer[] {i+1,j});
+
+        }
+        if(j>0){
+            neighbourList.add(new Integer[] {i,j-1});
+
+        }
+        if(j<matrix[0].length-1){
+            neighbourList.add(new Integer[] {i,j+1});
+
+        }
+
+
+        return neighbourList;
+    }
+
+
+}
